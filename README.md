a92ebcdc7e4dec1d05153421d47f46a2299f4ac8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-06-13 22:43:47 -0400

    working

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 > Run any command with package.json properties exposed as environment variables
 
+Imagine you want to use variables from the `package.json` in the script commands.
+Now you can. For example, let us say you want to commit and tag the git branch
+with the current `version` label. Install `with-package` and use it as first keyword
+in the command
+
+    npm install --save-dev with-package
+
+```json
+"scripts": {
+  "release": "with-package git commit -am pkg.version && with-package git tag pkg.version"
+},
+```
+
+Just refer to any primitive property of `package.json` as `pkg.<property name>`. Objects and
+arrays are not supported. If you need to debug the substitution, add `--debug` or `-d` flag
+to the command.
+
+    with-package echo this is pkg.description --debug
+
 ## Small print
 
 Author: Gleb Bahmutov &copy; 2015
--- bin/with-package.js
+++ bin/with-package.js
@@ -1,4 +1,79 @@
 #!/usr/bin/env node
 
 console.log('running with-package in', process.cwd());
-var pkg = require('./package.json');
+var join = require('path').join;
+var fullPackagePath = join(process.cwd(), 'package.json');
+var pkg = require(fullPackagePath);
+var quote = require('quote');
+
+function noop() {};
+function isDebug(args) {
+  return args.some(function (arg) {
+    return arg === '--debug' || arg === '-d';
+  });
+}
+
+var log = isDebug(process.argv) ? console.log.bind(console) : noop;
+
+function isPrimitive(x) {
+  return typeof x === 'string' ||
+    typeof x === 'number' ||
+    typeof x === 'boolean';
+}
+console.assert(isPrimitive(true), 'true is primitive');
+console.assert(isPrimitive(42), '42 is primitive');
+console.assert(isPrimitive('foo'), '"foo" is primitive');
+console.assert(!isPrimitive({}), '{} is NOT primitive');
+console.assert(!isPrimitive([]), '[] is NOT primitive');
+
+var primitives = Object.create(process.env);
+Object.keys(pkg).forEach(function (key) {
+  var value = pkg[key];
+  if (isPrimitive(value)) {
+    primitives[key] = value;
+  }
+});
+
+log(primitives);
+
+var args = process.argv.slice(2);
+if (!args.length) {
+  return;
+}
+
+function escape(value) {
+  if (typeof value === 'string' && value.length) {
+    return quote(value);
+  }
+}
+
+log('arguments', args);
+var replacedArgs = args.map(function (arg) {
+  if (/^pkg\./.test(arg)) {
+    var property = arg.substr(4);
+    console.log(property);
+    // return escape(primitives[property] || '');
+    return primitives[property] || '';
+  } else {
+    return arg;
+  }
+});
+log('replaced arguments', replacedArgs);
+
+var spawn = require('child_process').spawn;
+var proc = spawn(replacedArgs[0], replacedArgs.slice(1), {
+  env: process.env
+});
+
+proc.stdout.on('data', function (chunk) {
+  process.stdout.write(chunk);
+});
+
+proc.stderr.on('data', function (chunk) {
+  process.stderr.write(chunk);
+});
+
+proc.on('close', function (code) {
+  log('child process exited with code ' + code);
+});
+
